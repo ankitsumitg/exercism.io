ecc45315db61651b43b668000065a02eb25c9193
Ankit Gupta <ankitsumitg@gmail.com>
2021-04-15 21:27:29 +0530

    luhn

---
+++ rust/luhn/src/lib.rs
@@ -0,0 +1,30 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let mut digs:Vec<i32> = Vec::new();
+    for c in code.chars(){
+        if c.is_digit(10){
+            digs.push(c.to_digit(10).unwrap() as i32);
+        }
+        else if c.is_whitespace(){
+            continue;
+        }
+        else{
+            return false
+        }
+    }
+    if digs.len()%2 == 1{
+        digs.insert(0,0);
+    }
+    let mut sum =0;
+    for (pos,val) in digs.iter().enumerate(){
+        let mut v: i32 = *val;
+        if pos%2 == 0{
+            v = v*2;
+            if v > 9{
+                v -= 9;
+            }
+        }
+        sum += v;
+    }
+    sum%10 == 0 && !{digs.len() == 2 && sum == 0}
+}
