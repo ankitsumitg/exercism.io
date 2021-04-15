770c725a5d3612b0f89612d3b642061364dd7f61
Ankit Gupta <ankitsumitg@gmail.com>
2021-04-15 22:02:22 +0530

    update

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,30 +1,28 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
     let mut digs:Vec<i32> = Vec::new();
-    for c in code.chars(){
+    for c in code.replace(" ","").chars(){
         if c.is_digit(10){
             digs.push(c.to_digit(10).unwrap() as i32);
-        }
-        else if c.is_whitespace(){
-            continue;
         }
         else{
             return false
         }
     }
-    if digs.len()%2 == 1{
-        digs.insert(0,0);
-    }
-    let mut sum =0;
-    for (pos,val) in digs.iter().enumerate(){
-        let mut v: i32 = *val;
-        if pos%2 == 0{
-            v = v*2;
-            if v > 9{
-                v -= 9;
-            }
+    digs
+    .iter()
+    .rev()
+    .enumerate()
+    .map(|(pos,value)|
+    if pos&1 == 1{
+        if (*value)*2>9{
+            (*value)*2 - 9
         }
-        sum += v;
-    }
-    sum%10 == 0 && !{digs.len() == 2 && sum == 0}
+        else{
+            (*value)*2
+        }
+    } else {
+        *value
+    })
+    .sum::<i32>()%10 == 0 && digs.len() >1
 }
